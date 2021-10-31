beec8d68220440a5e475856a4a3ccb38934858d7
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-10-31 23:17:43 +0530

    Add files via upload

--- sample.php
+++ sample.php
@@ -2,6 +2,32 @@
     <body>
 
 <p>Hello</p>
+<?php
+/*
+* import checksum generation utility
+* You can get this utility from https://developer.paytm.com/docs/checksum/
+*/
+require_once("PaytmChecksum.php");
 
+$orderId=$_POST["ORDERID"];
+$paytmParams = array();
+
+$paytmParams["body"] = array(
+    "requestType"   => "Payment",
+    "mid"           => "nbCBZo84436307724140",
+    "websiteName"   => "BlackStoneGameDevelopement",
+    "orderId"       => $orderId,
+    "callbackUrl"   => "http://blackstonegamedevelopment.in",
+    "txnAmount"     => array(
+        "value"     => "1.00",
+        "currency"  => "INR",
+    ),
+    "userInfo"      => array(
+        "custId"    => "CUST_001",
+    ),
+);
+
+
+?>
 </body>
 </html>

eb1955f5caaa6eebafc592fb6be1074fd7b7431c
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-10-31 23:02:05 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -21,7 +21,17 @@ $paytmChecksum = PaytmChecksum::generateSignature($paytmParams, 'HsKuY%63DZkHISO
 $verifySignature = PaytmChecksum::verifySignature($paytmParams, 'HsKuY%63DZkHISOz', $paytmChecksum);
 echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
 echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
+        /* initialize JSON String */  
+$body = "{"\mid\":"\YOUR_MID_HERE\","\orderId\":"\.$_POST["ORDERID"].\"}";
 
+/**
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+$paytmChecksum = PaytmChecksum::generateSignature($body, 'HsKuY%63DZkHISOz');
+$verifySignature = PaytmChecksum::verifySignature($body, 'HsKuY%63DZkHISOz', $paytmChecksum);
+echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
+echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
 
 
 ?>

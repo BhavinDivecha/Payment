a6b07910f6543c47e9ef4aba1108ce508391778f
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-01 00:13:27 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -37,13 +37,39 @@ $paytmParam["ORDERID"] = $orderId;
 * Generate checksum by parameters we have
 * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
 */
-$paytmChecksum = PaytmChecksum::generateSignature($paytmParam, 'HsKuY%63DZkHISOz');
-$verifySignature = PaytmChecksum::verifySignature($paytmParam, 'HsKuY%63DZkHISOz', $paytmChecksum);
+// $paytmChecksum = PaytmChecksum::generateSignature($paytmParam, 'HsKuY%63DZkHISOz');
+// $verifySignature = PaytmChecksum::verifySignature($paytmParam, 'HsKuY%63DZkHISOz', $paytmChecksum);
+
+// $paytmParams["head"] = array(
+//     "signature"    => $paytmChecksum
+// );
+// echo "\n".$paytmChecksum;
+
+/*
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"], JSON_UNESCAPED_SLASHES), "HsKuY%63DZkHISOz");
 
 $paytmParams["head"] = array(
-    "signature"    => $paytmChecksum
+    "signature"    => $checksum
 );
-echo "\n".$paytmChecksum;
+echo "\n".$checksum;
+$post_data = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
+
+/* for Staging */
+$url = "https://securegw-stage.paytm.in/theia/api/v1/initiateTransaction?mid=nbCBZo84436307724140&orderId=".$orderId;
+
+/* for Production */
+// $url = "https://securegw.paytm.in/theia/api/v1/initiateTransaction?mid=nbCBZo84436307724140&orderId=".$orderId;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
+$response = curl_exec($ch);
+print_r($response);
 ?>
 </body>
 </html>

d7cb3a4a18170b6f8a6d2d54997c53733ce8e2d1
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-10-31 23:19:43 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -10,22 +10,7 @@
 require_once("PaytmChecksum.php");
 
 $orderId=$_POST["ORDERID"];
-$paytmParams = array();
-
-$paytmParams["body"] = array(
-    "requestType"   => "Payment",
-    "mid"           => "nbCBZo84436307724140",
-    "websiteName"   => "BlackStoneGameDevelopement",
-    "orderId"       => $orderId,
-    "callbackUrl"   => "http://blackstonegamedevelopment.in",
-    "txnAmount"     => array(
-        "value"     => "1.00",
-        "currency"  => "INR",
-    ),
-    "userInfo"      => array(
-        "custId"    => "CUST_001",
-    ),
-);
+echo $orderId;
 
 
 ?>

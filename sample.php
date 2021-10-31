b1877f6489ccb36361665030ea228cf982be9579
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-10-31 23:22:31 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -12,7 +12,11 @@ require_once("PaytmChecksum.php");
 $orderId=$_POST["ORDERID"];
 echo $orderId;
 $paytmParams = array();
-
+$paytmParams["body"] = array(
+        "requestType"   => "Payment",
+        "mid"           => "nbCBZo84436307724140",
+        "websiteName"   => "BlackStoneGameDevelopement"
+    );
 ?>
 </body>
 </html>

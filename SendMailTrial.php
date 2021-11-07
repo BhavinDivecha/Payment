a7a448a437cfd63830e5cd79cb5158228a2b1d2d
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-08 00:58:31 +0530

    Update SendMailTrial.php

--- SendMailTrial.php
+++ SendMailTrial.php
@@ -1,79 +1,28 @@
 <?php
 
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- * This uses traditional id & password authentication - look at the gmail_xoauth.phps
- * example to see how to use XOAUTH2.
- * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
- */
 
-//Import PHPMailer classes into the global namespace
-use PHPMailer;
-use SMTP;
-
-require 'PHPMailerAutoload.php';
-
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
-
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-
-//Enable SMTP debugging
-//SMTP::DEBUG_OFF = off (for production use)
-//SMTP::DEBUG_CLIENT = client messages
-//SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-//Use `$mail->Host = gethostbyname('smtp.gmail.com');`
-//if your network does not support SMTP over IPv6,
-//though this may cause issues with TLS
-
-//Set the SMTP port number:
-// - 465 for SMTP with implicit TLS, a.k.a. RFC8314 SMTPS or
-// - 587 for SMTP+STARTTLS
-$mail->Port = 465;
-
-//Set the encryption mechanism to use:
-// - SMTPS (implicit TLS on port 465) or
-// - STARTTLS (explicit TLS on port 587)
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = '3idevelopers2021@gmail.com';
-
-//Password to use for SMTP authentication
-$mail->Password = 'rachsbd123';
-
-//Set who the message is to be sent from
-//Note that with gmail you can only use your account address (same as `Username`)
-//or predefined aliases that you have configured within your account.
-//Do not use user-submitted addresses in here
-$mail->setFrom('3idevelopers2021@gmail.com', 'First Last');
-
-//Set who the message is to be sent to
-$mail->addAddress('bhavin.divecha09@gmail.com, 'John Doe');
-
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-
-
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
-}
+require 'phpmailer/class.phpmailer.php';
+  $mail = new PHPMailer;
+  $mail->IsSMTP();        //Sets Mailer to send message using SMTP
+  $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts
+  $mail->Port = '80';        //Sets the default SMTP server port
+  $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
+  $mail->Username = '3idevelopers2021@gmail.com';     //Sets SMTP username
+  $mail->Password = 'rachsbd123';     //Sets SMTP password
+  $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
+  $mail->From = '3idevelopers2021@gmail.com';     //Sets the From email address for the message
+  $mail->FromName = 'Bhavin';    //Sets the From name of the message
+  $mail->AddAddress('bhavin.divecha09@gmail.com', 'Name');//Adds a "To" address
+  $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
+  $mail->IsHTML(true);       //Sets message type to HTML    
+  $mail->Subject = 'Hello';    //Set s the Subject of the message
+  $mail->Body = 'Trial Message';    //An HTML or plain text message body
+  if($mail->Send())        //Send an Email. Return true on success or false on error
+  {
+   $error = '<label class="text-success">Thank you for contacting us</label>';
+  }
+  else
+  {
+   $error = '<label class="text-danger">There is an Error</label>';
+  }
+?>

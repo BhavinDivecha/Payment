6f5b059a74571893c00cea32427c850618e8acc9
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-08 00:23:46 +0530

    Update mail_function.php

--- mail_function.php
+++ mail_function.php
@@ -6,7 +6,7 @@
 		$message_body = " Your OTP for Ludo Classic Pro is : ".$otp;
 		$mail = new PHPMailer();
 	   //$mail->AddReplyTo('tutorialspoint2016@gmail.com','Technical Suneja');
-                                $mail->SetFrom('tutorialspoint2016@gmail.com','Technical Suneja');
+                                $mail->SetFrom('blackston404@gmail.com','Bhavin Divecha');
                                 $mail->AddAddress($email);
                                 $mail->Subject= "OTP ".$otp;
                                 $mail->MsgHTML($message_body);

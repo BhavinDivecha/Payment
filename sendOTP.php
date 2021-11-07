8de5a5fc6b63c7b83f2d882c5237f8556af05f3a
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-08 00:25:44 +0530

    Update sendOTP.php

--- sendOTP.php
+++ sendOTP.php
@@ -1,7 +1,7 @@
 <?php
 	include "mail_function.php";
 	$email='Bhavin.divecha09@gmail.com';//$_POST["email"];
-	$otp=
+	$otp=$_POST["OTP"];
 	$message=' Your OTP for verify Your Email in BMGL : '.$otp;
 	$mail_status=sendOTP($email,$otp);
 if($mail_status==1){

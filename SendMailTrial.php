3d5375a6e043e8e9181aa6f260fe3a43c0493992
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-08 01:46:14 +0530

    Update SendMailTrial.php

--- SendMailTrial.php
+++ SendMailTrial.php
@@ -10,10 +10,10 @@ require 'phpmailer/class.phpmailer.php';
   $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts
   $mail->Port = '80';        //Sets the default SMTP server port
   $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
-  $mail->Username = '3idevelopers2021@gmail.com';     //Sets SMTP username
-  $mail->Password = 'rachsbd123';     //Sets SMTP password
+  $mail->Username = 'blackston404@gmail.com';     //Sets SMTP username
+  $mail->Password = 'bhbh123123';     //Sets SMTP password
   $mail->SMTPSecure = 'tls';       //Sets connection prefix. Options are "", "ssl" or "tls"
-  $mail->From = '3idevelopers2021@gmail.com';     //Sets the From email address for the message
+  $mail->From = 'blackston404@gmail.com';     //Sets the From email address for the message
   $mail->FromName = 'Bhavin';    //Sets the From name of the message
   $mail->AddAddress('bhavin.divecha09@gmail.com', 'Name');//Adds a "To" address
   $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters

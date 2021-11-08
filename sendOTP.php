23bc3c0457793efecc4fa23178445e80ca6e738d
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2021-11-08 14:57:07 +0530

    OTP Trial

--- sendOTP.php
+++ sendOTP.php
@@ -1,13 +1,77 @@
 <?php
-	include "mail_function.php";
-	$email='Bhavin.divecha09@gmail.com';//$_POST["email"];
-	$otp=$_POST["OTP"];
-	$message=' Your OTP for verify Your Email in BMGL : '.$otp;
-	$mail_status=sendOTP($email,$otp);
-if($mail_status==1){
-	echo "Succ";
+
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ * This uses traditional id & password authentication - look at the gmail_xoauth.phps
+ * example to see how to use XOAUTH2.
+ * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+ */
+
+//Import PHPMailer classes into the global namespace
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+$email=$_POST["EMAILID"];
+$otp=$_POST["OTP"];
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+//SMTP::DEBUG_OFF = off (for production use)
+//SMTP::DEBUG_CLIENT = client messages
+//SMTP::DEBUG_SERVER = client and server messages
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+//Use `$mail->Host = gethostbyname('smtp.gmail.com');`
+//if your network does not support SMTP over IPv6,
+//though this may cause issues with TLS
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption mechanism to use - STARTTLS or SMTPS
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = 'blackston404@gmail.com';
+
+//Password to use for SMTP authentication
+$mail->Password = 'bhbh123123';
+
+//Set who the message is to be sent from
+$mail->setFrom('blackston404@gmail.com', 'Bhavin Divecha');
+
+//Set who the message is to be sent to
+$mail->addAddress($email, 'Patel');
+
+//Set the subject line
+$mail->Subject = 'OTP '.$otp;
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+//Replace the plain text body with one created manually
+$mail->Body = 'Your OTP for verify Your Email in Ludo Classic : '.$otp;
+
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
-else{
-	echo "Error";
-}	
 ?>

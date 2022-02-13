cc7e19c68330a09259f120bca98374b23697d639
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2022-02-13 14:35:11 +0530

    Update payUHash.php

--- payUHash.php
+++ payUHash.php
@@ -108,7 +108,7 @@ function checkNull($value) {
 $output=getHashes($_POST["txnid"], $_POST["amount"], $_POST["productinfo"], $_POST["firstname"], $_POST["email"], $_POST["user_credentials"], $_POST["udf1"], $_POST["udf2"], $_POST["udf3"], $_POST["udf4"], $_POST["udf5"],$_POST["offerKey"],$_POST["cardBin"]);
 
 
-echo str_replace('"', '', json_encode($output));
+$hash = str_replace('"', '', json_encode($output));
 //echo json_encode($output);
 
 ?>
@@ -119,10 +119,10 @@ echo str_replace('"', '', json_encode($output));
 <input type="text" name="amount" value=<?php echo $_POST["amount"]?> />
 <input type="email" name="email" value=<?php echo $_POST["email"]?> />
 <input type="text" name="firstname" value=<?php echo $_POST["firstname"]?> />
-<input type="text" name="phone" value="7021659118" />
+<input type="text" name="phone" value=<?php echo $_POST["phone"]?> />
 <input type="hidden" name="surl" value="https://bgdlegend.herokuapp.com/success.php" />
 <input type="hidden" name="furl" value="https://bgdlegend.herokuapp.com/failed.php" />
-<input type="text" name="hash" value="eabec285da28fd0e3054d41a4d24fe9f7599c9d0b66646f7a9984303fd6124044b6206daf831e9a8bda28a6200d318293a13d6c193109b60bd4b4f8b09c90972" />
+<input type="text" name="hash" value=<?php echo $hash ?> />
 <input type="submit" value="submit"> </form>
 </body>
 </html>

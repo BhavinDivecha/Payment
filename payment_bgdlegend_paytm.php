1a7271c76e7a66714a83e52ecb979050dc6a3f87
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2022-05-31 23:02:39 +0530

    Update payment_bgdlegend_paytm.php

--- payment_bgdlegend_paytm.php
+++ payment_bgdlegend_paytm.php
@@ -48,6 +48,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
 $response = curl_exec($ch);
+print($response);
 $result=json_decode($response, true);
         if ($result['body']['resultInfo']['resultStatus']=="S"){
         echo $result['body']['txnToken'];
